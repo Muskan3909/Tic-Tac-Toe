cc292e591bc1de138fcf71da0899b8c094d0b3ab
Muskan3909 <kushwahamuskan97.com>
2026-04-03 16:07:16 +0530

    Fix Nakama startup flags

--- start.sh
+++ start.sh
@@ -6,6 +6,4 @@ exec /nakama/nakama \
   --name nakama1 \
   --database.address "$DB_URL" \
   --socket.server_key defaultkey \
-  --socket.port 10000 \
-  --api.port 10000 \
   --runtime.path /nakama/data/modules
